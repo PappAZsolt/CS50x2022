bc92d5403e62a525c32fee4fa840408510d026a8
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-02-20 13:41:19 +0000

    Sun, Feb 20, 2022, 1:41 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,7 +9,12 @@ int main(void)
         x = get_int("Height: \n");
     } while (x < 1);
     printf("%i\n", x);
-    for (int i = 0; i < x; i++){
-        printf("#");
+    for (int i = 0; i < x; i++)
+    {
+        {
+            for (int j = 0; j < x; j++)
+                printf("#");
+        }
+        printf("\n");
     }
 }
