fda71b72a2e05f0ae8c959fd2b6abb886c255159
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-03-06 11:53:46 +0000

    Sun, Mar 6, 2022, 11:53 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,9 @@ int main(void)
 
   for (int i = 1; i <= x; i++)
   {
-    for (int j = 1; j <= i; j++)
+    for (int j = 1; j <= x - i; j++)
+      printf(" ");
+    for (int d = 1; d <= i; d++)
       printf("#");
     printf("\n");
   }
