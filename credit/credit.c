94085d8ec7dc72e73f27dc0d9e1813c60ad68efc
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-03-06 18:58:24 +0000

    Sun, Mar 6, 2022, 6:58 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,42 +3,55 @@
 
 bool AE(long long x)
 {
-    if (x == 34 || x == 37)
-      return true;
+  if (x == 34 || x == 37)
+  {
+    return true;
     return false;
   }
+
   return false;
 }
 
 bool MC(long long x)
 {
-    if (x == 51 || x == 52 || x == 53 || x == 54 || x == 55)
-      return true;
+  if (x == 51 || x == 52 || x == 53 || x == 54 || x == 55)
+  {
+    return true;
     return false;
   }
+
   return false;
 }
 
 bool Visa(long long x)
 {
-    if (x == 13 || x == 1)
-      return true;
+  if (x == 13 || x == 1)
+  {
+    return true;
     return false;
   }
+
   return false;
 }
 
-int type(int x){
-  if (x == )
-}
+/* int type(int x)
+{
+  if (x ==)
+} */
 
-int main(){
+int main()
+{
   long long x;
-  do {
+  do
+  {
     x = get_long("Number: ");
-  }while (x < 1);
+  } while (x < 1);
   long long aux = x;
-
-  
-
+  int cnt = 2;
+  while (aux)
+  {
+    aux--;
+    cnt++;
+  }
+  printf("%i", cnt);
 }
