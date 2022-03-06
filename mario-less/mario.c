d7ff57ae1adde391bf894b4e4cb95240c98b30cc
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-03-06 11:44:00 +0000

    Sun, Mar 6, 2022, 11:44 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -8,11 +8,11 @@ int main(void)
   {
     x = get_int("Height: \n ");
   } while (x < 1 || x > 8);
-}
 
-for (int i = 0; i < x; i++)
-{
-  for (int j = 0; j < x; j++)
-    printf("#");
-  printf('\n');
+  for (int i = 1; i <= x; i++)
+  {
+    for (int j = 1; j <= i; j++)
+      printf("#");
+    printf("\n");
+  }
 }
