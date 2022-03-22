dfa837bab3f836ebd73922475695f079a044cb28
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-03-22 19:25:23 +0000

    Tue, Mar 22, 2022, 7:25 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,15 +1,21 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
+int count_letters(string text);
 
 int main(void)
 {
   string text = get_string("Text: ");
-  printf("%s\n", text);
+  int letters = count_letters(text);
+  printf("%i\n", letters);
 }
 
 int count_letters(string text)
 {
   int cnt = 0;
-  
+  for (int i = 0, n = strlen(text); i < n; i++)
+    cnt++;
+  return cnt;
 }
