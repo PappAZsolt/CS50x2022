94108d7e4e8749345d415dd83d384cf512d2b960
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-03-24 16:21:08 +0000

    Thu, Mar 24, 2022, 4:21 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+bool only_digits(string s);
+
 int main(int argc, string argv[])
 {
-  if (only_digits(argv[1]) == 0 || argv[2 ])
-   {
-     printf("Usage: ./caesar key");
-     return 1;
-   }
-
-
+  if (only_digits(argv[1]) == 0 || argv[2] != '\0')
+  {
+    printf("Usage: ./caesar key");
+    return 1;
+  }
+  else
+  {
+    string plaintext = get_string("plaintext: \n");
+    printf("%s\n", plaintext);
+  }
 }
 
 bool only_digits(string s)
 {
   int lenght = strlen(s);
   for (int i = 0; i < lenght; i++)
-   if (isdigit(s[i]) == 0)
-    return false;
-   return true;
+    if (isdigit(s[i]) == 0)
+      return false;
+  return true;
 }
