b4e2bf3b16ccd4c4d7bae14c515898ab883fbafe
PappAZsolt <68129312+PappAZsolt@users.noreply.github.com>
2022-03-24 16:00:44 +0000

    Thu, Mar 24, 2022, 4:00 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,5 +10,7 @@ int main(int argc, string argv[])
 bool only_digits(string s)
 {
   int lenght = strlen(s);
-  for (int i = 0; i < lenght;)
+  for (int i = 0; i < lenght; i++)
+   if (isdigit(s[i]) == 0)
+    return 
 }
