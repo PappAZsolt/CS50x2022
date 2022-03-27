a23fa39cfb555a9ffce4c56b2475b2285346b0b3
PappAZsolt <pappzsolt918@gmail.com>
2022-03-27 14:04:24 +0300

    C

---
+++ week1/mario-more/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    do
+    {
+        x = get_int("Height: \n ");
+    } while (x < 1 || x > 8);
+
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= x - i; j++)
+            printf(" ");
+        for (int d = 1; d <= i; d++)
+            printf("#");
+
+        printf("  ");
+
+        for (int k = 1; k <= i; k++)
+            printf("#");
+        printf("\n");
+    }
+}
